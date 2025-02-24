9a158e01a528cd5fa5e6acaf4bd849ee4d156bcb
AnaM-coder <andraana@students.zhaw.ch>
2025-02-24 21:39:30 +0100

    a

--- Start.py
+++ Start.py
@@ -4,3 +4,8 @@ import pandas as pd
 st.title("Meine erste Streamlit App")
 
 st.write("Diese App wurde von Ana Maria Andrade entwickelt.")
+<<<<<<< HEAD
+=======
+
+st.write ("andraana@students.zhaw.ch")
+>>>>>>> fda848d (a)
