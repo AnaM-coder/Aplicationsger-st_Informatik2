f91d762d1d0364315b037cd103c40a8e9f624bab
AnaM-coder <andraana@students.zhaw.ch>
2025-03-08 23:22:24 +0100

    home

--- Start.py
+++ Start.py
@@ -10,6 +10,3 @@ st.info("""Bitte beachte, dass dies nur eine Schätzung ist und keine profession
 st.info("""Wenn du Fragen zu deinem Idealgewicht hast, wende dich bitte an deinen Arzt oder Ernährungsberatung!""")
 
 st.write("Diese App wurde von Ana Maria Andrade (andraana@students.zhaw.ch) im Rahmen der Moduls 'BMLD Informatik 2' an der ZHAW entwickelt.")
-
-if st.button("Home"):
-    st.switch_page("idealgewicht Rechner.py")
