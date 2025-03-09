6ac57f9c9bf96273ecba95e83700e17f9c6ac0c3
AnaM-coder <andraana@students.zhaw.ch>
2025-03-09 17:36:22 +0100

    korrektur info

--- Start.py
+++ Start.py
@@ -8,6 +8,6 @@ st.markdown("Dieser Rechner verwendet die Devine-Formel zur Berechnung des Ideal
 
 st.info("""Deine persönliche Ergebnisse können variieren, basierend auf weiteren Gesundheitsfaktoren!""")
 st.info("""Bitte beachte, dass dies nur eine Schätzung ist und keine professionelle medizinische Beratung ersetzt!""")
-st.info("""Wenn du Fragen zu deinem Idealgewicht hast, wende dich bitte an deinen Arzt oder Ernährungsberatung!""")
+st.info("""Wenn du Fragen zu deinem Idealgewicht hast, wende dich bitte an einen Arzt oder eine Ernährungsberatung!""")
 
 st.write("Diese App wurde von Ana Maria Andrade (andraana@students.zhaw.ch) im Rahmen der Moduls 'BMLD Informatik 2' an der ZHAW entwickelt.")
